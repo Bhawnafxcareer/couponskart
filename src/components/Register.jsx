02027091021ae693328445671aad1d629c4807d8
bhawnafxcareer <bhawna@fxcareers.com>
2025-04-24 12:53:19 +0530

    otp field add in register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
-import ThemeToggle from "./ThemeToggle";
+
 
 const Register = () => {
+  const [showOTP, setShowOTP] = useState(false);
+  const [otp, setOtp] = useState(new Array(6).fill(""));
+
+  const handleSendOtp = () => {
+    setShowOTP(true);
+    // Here you'd call your API to actually send the OTP to the email
+  };
+
+  const handleOtpChange = (element, index) => {
+    if (/^\d?$/.test(element.value)) {
+      const newOtp = [...otp];
+      newOtp[index] = element.value;
+      setOtp(newOtp);
+
+      // Move to next input if valid
+      if (element.nextSibling && element.value) {
+        element.nextSibling.focus();
+      }
+    }
+  };
   return (
     <>
       {/* ================================== Account Page Start =========================== */}
@@ -33,12 +55,12 @@ const Register = () => {
         <div className="account__right padding-t-120 flx-align">
       
             <div className="account-content">
-            <h4 className="account-content__title mb-48 text-capitalize">
+            <h4 className="account-content__title mb-4 text-capitalize">
               Create A Free Account
             </h4>
             <form action="#">
               <div className="row gy-4">
-                <div className="col-12">
+                <div className="col-md-12">
                   <label
                     htmlFor="name"
                     className="form-label mb-2 font-18 font-heading fw-600"
@@ -57,7 +79,7 @@ const Register = () => {
                     </span>
                   </div>
                 </div>
-                <div className="col-12">
+                <div className="col-md-12">
                   <label
                     htmlFor="email"
                     className="form-label mb-2 font-18 font-heading fw-600"
@@ -72,16 +94,66 @@ const Register = () => {
                       placeholder="infoname@mail.com"
                     />
                     <span className="input-icon">
-                      <img src="assets/images/icons/envelope-icon.svg" alt="" />
+                     <button className="btn-main p-1 text-white rounded font-14
+                     "   onClick={handleSendOtp}>Send OTP</button>
                     </span>
                   </div>
                 </div>
-                <div className="col-12">
+               {/* OTP Input Section */}
+              {showOTP && (
+                <>
+                  <div className="col-12">
+                    <label className="form-label mb-2">Enter OTP</label>
+                    <div className="d-flex gap-3 justify-content-center">
+                      {otp.map((digit, idx) => (
+                        <input
+                          key={idx}
+                          type="text"
+                          inputMode="numeric"
+                          maxLength={1}
+                          value={digit}
+                          onChange={(e) => handleOtpChange(e.target, idx)}
+                          className="form-control text-center"
+                          style={{ width: "50px", fontSize: "20px" }}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <button type="button" className="btn btn-main w-100 pill mt-3">
+                      Verify OTP
+                    </button>
+                  </div>
+                </>
+              )}
+                <div className="col-md-6">
                   <label
                     htmlFor="your-password"
                     className="form-label mb-2 font-18 font-heading fw-600"
                   >
                     Password
+                  </label>
+                  <div className="position-relative">
+                    <input
+                      type="password"
+                      className="common-input common-input--bg common-input--withIcon"
+                      id="your-password"
+                      placeholder="6+ characters, 1 Capital letter"
+                    />
+                    <span
+                      className="input-icon toggle-password cursor-pointer"
+                      id="#your-password"
+                    >
+                      <img src="assets/images/icons/lock-icon.svg" alt="" />
+                    </span>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <label
+                    htmlFor="your-password"
+                    className="form-label mb-2 font-18 font-heading fw-600"
+                  >
+                    Confirm Password
                   </label>
                   <div className="position-relative">
                     <input
